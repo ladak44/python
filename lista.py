8030e66449574a3225d2a3b2dd6431b74d5b3927
MARCLAD <marcin.ladak@gmail.com>
2020-11-02 16:11:55 +0100

    na

--- lista.py
+++ lista.py
@@ -2,7 +2,8 @@ def main():
     # print(czy_wszystkie_dodatnie([1,2,3,-4]))
     #print(srednia([1,3,4,-20]))
     # print(skumulowana([1,2,4,4,7]))
-    print(suma_list([1,4,5],[5,2,1]))
+    # print(suma_list([1,4,5],[5,2,1]))
+    print_list([44,66,88,99])
 
 def suma_list(lista1,lista2):
     wynik = [None] * len(lista1)
@@ -34,6 +35,10 @@ def srednia(lista):
     return sum / len(lista)
     # lista = [1,3,3,-5]
     # print(len(lista))
+
+def print_list(lista):
+    for index, element in enumerate(lista):
+        print (index,element)
 
 if __name__ == '__main__':
     main()
