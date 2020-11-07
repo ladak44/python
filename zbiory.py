d5d7a55bc2c29f2662c5938d29f6b4ba2043d10c
MARCLAD <marcin.ladak@gmail.com>
2020-11-07 19:28:08 +0100

    na

--- zbiory.py
+++ zbiory.py
@@ -7,12 +7,47 @@ def main():
     baza_danych = {"1": ["Marcin","Ladak",45],
                    "2": ["Eliza","Ladak",46]}
 
-    print(baza_danych["2"])
-    # print(1 in baza_danych)
-    # print(f"xxxxxx {zbior}")
+
+    baza_danych["2"] = ["aaaa","eeeee","wwwww","qqq","cccc"]
+    baza_danych["3"] = ["lola"]
+    baza_danych["a"] = ["ola lola"]
+
+    # print(baza_danych["1"])
+    # print(baza_danych)
+    # for i in baza_danych.keys():
+    #     print(i)
+    #
+    # for w in baza_danych.values():
+    #     print(w)
+
+    # for i,w in baza_danych.items():
+    #     print(i)
+    #     print(w)
+
+def palindrom(slowo):
+    pass
+    n = len(slowo)//2
+    for i in range(0,n):
+        k = -i-1
+        if slowo[i] != slowo[k]:
+            print(f"f1 {slowo[i]}")
+            print(f"f2 {slowo[k]}")
+            return False
+    print("Palindrom")
+    return True
+
+def test():
+    slowo = "kajak"
+    for i in range(0,len(slowo)//2):
+        print(f"minus : {slowo[-i-1]}")
+        print(f"plus : {slowo[i]}")
+        print("==========")
 
 
-    # print(2 in zbior)
+def test2(slowo):
+    return slowo == slowo[::-1]
 
 if __name__ == '__main__':
-    main()
+    # palindrom("kajak")
+    print(test2("kajak"))
+    # main()
